28129442589e652a3cf8e39a3d8a6d7acd8b8f6b
James Sapp <jameseisapp@gmail.com>
2021-01-12 16:50:32 -0600

    commit tree 949a6b1462d47618d92ea9f92d9e8ab4d6b610c0

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,23 @@
 class Ship
+
+    attr_reader :name, :type, :booty
+
+    @@ships = []
+
+    def initialize(args)
+        @name = args[:name]
+        @type = args[:type]
+        @booty = params[:booty]
+        @@ships << self 
+    end
+
+    def self.all
+        @@ships
+    end
+
+    def self.clear
+        @@ships = []
+    end
+
+
 end
